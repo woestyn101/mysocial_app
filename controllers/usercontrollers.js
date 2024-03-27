2407b627a8e9c0c490f0718bffd63729fdb05d3f
Marius Du Plooy <woestyn101@gmail.com>
2024-03-27 08:03:48 -0400

    get single user

--- controllers/usercontrollers.js
+++ controllers/usercontrollers.js
@@ -27,4 +27,23 @@ const getUsers = async (req, res) => {
       
    }
 
-module.exports = {getUsers, createUser}
+   //get a single user
+const getUser = async (req, res) => {
+
+             
+           const {id} = req.params;
+           if(!mongoose.Types.ObjectId.isValid(id)){
+            return   res.status(404).json({error: "no such user"});
+        }
+           const user = await User.findById(id);
+
+           if (!user){
+            return   res.status(404).json({error: "no such user"});
+           }
+
+        res.status(200).json(user);
+   
+      
+   }
+
+module.exports = {getUsers, getUser, createUser}
--- routes/userroutes.js
+++ routes/userroutes.js
@@ -1,11 +1,13 @@
 const mongoose = require("mongoose");
 const express = require("express");
-const {createUser, getUsers} = require('../controllers/usercontrollers');
+const {createUser, getUsers, getUser} = require('../controllers/usercontrollers');
 
 const router = express.Router();
 
 router.post('/', createUser);
 
 router.get('/', getUsers);
+
+router.get('/:id', getUser);
 
 module.exports = router;
